daa2e910bdaf52b4e8bb28f3f53ac2bb19c3d8dc
Simon Leistikow <s_leis06@uni-muenster.de>
2018-02-14 12:53:54 +0100

    Initial commit

---
+++ sv.php
@@ -0,0 +1,47 @@
+<!DOCTYPE HTML>
+<!--
+	Editorial by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+    <?php
+    include("common/head.html");
+    ?>
+
+	<body>
+
+		<!-- Wrapper -->
+		<div id="wrapper">
+
+			<!-- Main -->
+			<div id="main">
+				<div class="inner">
+
+					<!-- Header -->
+				    <?php
+                    include("common/header.html");
+                    ?>
+                    
+                    <!-- Content -->
+				    <?php
+                    include("content/".basename(__FILE__, ".php").".html");
+                    ?>
+
+				</div>
+			</div>
+
+			<!-- Sidebar -->
+            <?php
+            include("common/sidebar.html");
+            ?>
+
+		</div>
+
+		<!-- Scripts -->
+        <?php
+        include("common/scripts.html");
+        ?>
+
+	</body>
+</html>
